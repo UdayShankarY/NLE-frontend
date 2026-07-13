d93b4cf933704582a06004f098a94a781a54be30
Uday Shankar <shankarudaay@gmail.com>
2026-07-14 02:56:42 +0530

    add vercel jason

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 const API_BASE_URL = import.meta.env.VITE_API_URL?.replace(/\/$/, '');
-
+console.log("VITE_API_URL =", API_BASE_URL);
 export function getApiUrl(path: string) {
   if (!path.startsWith('/')) {
     path = `/${path}`;
